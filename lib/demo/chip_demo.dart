76ec7d5bba091ec0fe08f447bc18bd9abd2a4b96
18278427910 <2956730565@qq.com>
2020-03-27 17:11:46 +0800

    删除小部件和分割部件

--- lib/demo/chip_demo.dart
+++ lib/demo/chip_demo.dart
@@ -6,6 +6,7 @@ class ChipDemo extends StatefulWidget {
 }
 
 class _ChipDemoState extends State<ChipDemo> {
+  List<String> _tag = ["Apple","Banana","Orange","Lemon"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,20 +17,20 @@ class _ChipDemoState extends State<ChipDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Wrap(//可换行
-            //  mainAxisAlignment: MainAxisAlignment.center,
-            direction: Axis.horizontal,//部件摆放方向，默认为水平方向
-            alignment: WrapAlignment.center,//对其方式
-            textDirection: TextDirection.ltr,
-            spacing: 15.0,//设置间距
-            runSpacing: 8.0,//行间距
+            Wrap(
+              //可换行
+              //  mainAxisAlignment: MainAxisAlignment.center,
+              direction: Axis.horizontal, //部件摆放方向，默认为水平方向
+              alignment: WrapAlignment.center, //对其方式
+              textDirection: TextDirection.ltr,
+              spacing: 15.0, //设置间距
+              runSpacing: 8.0, //行间距
               children: <Widget>[
-                 Chip(
+                Chip(
                   label: Text("History"),
                   elevation: 4.0,
                   labelPadding: EdgeInsets.all(5.0),
                 ),
-               
                 Chip(
                   label: Text("History"),
                   backgroundColor: Colors.green,
@@ -38,12 +39,10 @@ class _ChipDemoState extends State<ChipDemo> {
                   elevation: 4.0,
                   labelPadding: EdgeInsets.all(5.0),
                 ),
-              
                 Chip(
                   label: Text("宁浩"),
                   avatar: CircleAvatar(
-                      child: Text("皓"),
-                
+                    child: Text("皓"),
                   ),
                   labelStyle: TextStyle(color: Colors.white),
                   elevation: 4.0,
@@ -60,11 +59,47 @@ class _ChipDemoState extends State<ChipDemo> {
                   elevation: 4.0,
                   labelPadding: EdgeInsets.all(5.0),
                 ),
+                Chip(
+                  label: Text("Delect"),
+                  onDeleted: (){},
+                  deleteIcon: Icon(Icons.delete),//删除图标
+                  deleteIconColor: Colors.redAccent,//图标颜色
+                  deleteButtonTooltipMessage: "Remove this Tag",//长安图标提示语
+                ),
+                Divider(//分隔符
+                  color: Colors.grey,
+                  height: 22.0,//距离上部的距离
+                  indent: 16.0,//左边的缩进
+                  endIndent: 16.0,//右边的缩进
+                  thickness: 0.5,//分割线的高度
+                ),
+                Wrap(
+                  spacing: 12.0,
+                  children: _tag.map((tag){
+                    return Chip(
+                      label: Text(tag),
+                      onDeleted: (){
+                        setState(() {
+                          _tag.remove(tag);
+                        });
+                      },
+                      );
+                  }).toList(),
+                ),
               ],
             ),
           ],
         ),
+      
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.restore),
+        onPressed: (){
+          setState(() {
+            _tag = ["Apple","Banana","Orange","Lemon"];
+          });
+        }
+        ),
     );
   }
 }
