334d7fee695037aa0c6f30b64712e97acf3db29e
18278427910 <2956730565@qq.com>
2020-03-27 17:45:18 +0800

    ActionChip和FilterChip

--- lib/demo/chip_demo.dart
+++ lib/demo/chip_demo.dart
@@ -6,7 +6,12 @@ class ChipDemo extends StatefulWidget {
 }
 
 class _ChipDemoState extends State<ChipDemo> {
-  List<String> _tag = ["Apple","Banana","Orange","Lemon"];
+  List<String> _tag = ["Apple", "Banana", "Orange"];
+
+  String _action = "Nothing";
+
+  List<String> _selected = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,17 +19,19 @@ class _ChipDemoState extends State<ChipDemo> {
         title: Text("ChipDemo"),
       ),
       body: Container(
+        padding: EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Wrap(
               //可换行
               //  mainAxisAlignment: MainAxisAlignment.center,
-              direction: Axis.horizontal, //部件摆放方向，默认为水平方向
-              alignment: WrapAlignment.center, //对其方式
-              textDirection: TextDirection.ltr,
+              //   direction: Axis.horizontal, //部件摆放方向，默认为水平方向
+              //   alignment: WrapAlignment.center, //对其方式
+              //   textDirection: TextDirection.ltr,
               spacing: 15.0, //设置间距
               runSpacing: 8.0, //行间距
+
               children: <Widget>[
                 Chip(
                   label: Text("History"),
@@ -61,45 +68,104 @@ class _ChipDemoState extends State<ChipDemo> {
                 ),
                 Chip(
                   label: Text("Delect"),
-                  onDeleted: (){},
-                  deleteIcon: Icon(Icons.delete),//删除图标
-                  deleteIconColor: Colors.redAccent,//图标颜色
-                  deleteButtonTooltipMessage: "Remove this Tag",//长安图标提示语
+                  onDeleted: () {},
+                  deleteIcon: Icon(Icons.delete), //删除图标
+                  deleteIconColor: Colors.redAccent, //图标颜色
+                  deleteButtonTooltipMessage: "Remove this Tag", //长安图标提示语
                 ),
-                Divider(//分隔符
+                Divider(
+                  //分隔符
                   color: Colors.grey,
-                  height: 22.0,//距离上部的距离
-                  indent: 16.0,//左边的缩进
-                  endIndent: 16.0,//右边的缩进
-                  thickness: 0.5,//分割线的高度
+                  height: 22.0, //距离上部的距离
+                  //   indent: 16.0,//左边的缩进
+                  endIndent: 16.0, //右边的缩进
+                  //  thickness: 0.5,//分割线的高度
                 ),
                 Wrap(
                   spacing: 12.0,
-                  children: _tag.map((tag){
+                  children: _tag.map((tag) {
                     return Chip(
                       label: Text(tag),
-                      onDeleted: (){
+                      onDeleted: () {
                         setState(() {
                           _tag.remove(tag);
                         });
                       },
-                      );
+                    );
+                  }).toList(),
+                ),
+                Divider(
+                  //分隔符
+                  color: Colors.grey,
+                  height: 22.0, //距离上部的距离
+                  //   indent: 16.0,//左边的缩进
+                  endIndent: 16.0, //右边的缩进
+                  //   thickness: 0.5,//分割线的高度
+                ),
+                Container(
+                  width: double.infinity,
+                  child: Text("Action:$_action"),
+                ),
+                Wrap(
+                  spacing: 12.0,
+                  children: _tag.map((tag) {
+                    return ActionChip(
+                      label: Text(tag),
+                      onPressed: () {
+                        setState(() {
+                          _action = tag;
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
+                  Divider(
+                  //分隔符
+                  color: Colors.grey,
+                  height: 22.0, //距离上部的距离
+                  //   indent: 16.0,//左边的缩进
+                  endIndent: 16.0, //右边的缩进
+                  //   thickness: 0.5,//分割线的高度
+                ),
+                Container(
+                  width: double.infinity,
+                  child: Text("FliterAction:${_selected.toString()}"),
+                ),
+                Wrap(
+                  spacing: 12.0,
+                  children: _tag.map((tag) {
+                    return FilterChip(
+                      label: Text(tag),
+                      selected: _selected.contains(tag),
+                      onSelected: (value){
+                          setState(() {
+                            if (_selected.contains(tag)) {
+                              _selected.remove(tag);
+
+                            } else {
+                              _selected.add(tag);
+
+                            }
+                          });
+                      },
+                    );
                   }).toList(),
                 ),
               ],
+              
             ),
-          ],
+           
+              ],
         ),
-      
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.restore),
-        onPressed: (){
-          setState(() {
-            _tag = ["Apple","Banana","Orange","Lemon"];
-          });
-        }
-        ),
+          child: Icon(Icons.restore),
+          onPressed: () {
+            setState(() {
+              _tag = ["Apple", "Banana", "Orange"];
+              _selected = [];
+            });
+          }),
     );
   }
 }
