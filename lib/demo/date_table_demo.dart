661ee6aa6915802ff004c6c07acf511591c3c03d
18278427910 <2956730565@qq.com>
2020-03-28 14:57:16 +0800

    PaginatedDataTable带分页的DataTable

--- lib/demo/date_table_demo.dart
+++ lib/demo/date_table_demo.dart
@@ -1,73 +1,102 @@
 import 'package:flutter/material.dart';
 import '../model/post.dart';
 
+class PostDataSource extends DataTableSource {
+  final List<Post> _pos = posts;
+  int _selectCount = 0;
+  @override
+  int get rowCount => _pos.length; //一共有多少行
+
+  @override
+  int get selectedRowCount => _selectCount; //被选中的行数
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  DataRow getRow(int index) {//展示的数据
+    Post pos = _pos[index];
+    return DataRow.byIndex(index: index, cells: <DataCell>[
+      DataCell(Text(pos.title)),
+      DataCell(Text(pos.author)),
+      DataCell(Image.network(pos.imageUrl)),
+    ]);
+  }
+
+  void sort(getField(post),bool ascedning){
+     _pos.sort((a,b){
+        if(!ascedning){
+          final c = a;
+          a = b;
+          b = c;
+        }
+        final aValue = getField(a);
+        final bValue = getField(b);
+        return Comparable.compare(aValue, bValue);
+     });
+
+     notifyListeners();
+  }
+}
+
 class DateTableDemo extends StatefulWidget {
   @override
   _DateTableDemoState createState() => _DateTableDemoState();
 }
 
 class _DateTableDemoState extends State<DateTableDemo> {
-     int _sortColumnIndex;
-    bool _sortAscending = true;
+  int _sortColumnIndex;
+  bool _sortAscending = true;
+
+  final PostDataSource _postDataSource = PostDataSource();
   @override
   Widget build(BuildContext context) {
- 
     return Scaffold(
       appBar: AppBar(title: Text("DateTableDemo")),
       body: Container(
         padding: EdgeInsets.all(8.0),
-        child: ListView(
-          children: <Widget>[
-            DataTable(
-              sortColumnIndex: _sortColumnIndex,//排序的栏目
-              sortAscending: _sortAscending,
-              columns: [
+        child: ListView(children: <Widget>[
+          PaginatedDataTable(
+            header: Text("Post"),
+            source: _postDataSource,
+            rowsPerPage: 5,//默认每页显示人数为10
+            sortColumnIndex: _sortColumnIndex, //排序的栏目
+            sortAscending: _sortAscending,
+
+            columns: [
               DataColumn(
                 label: Text("Title"),
-                onSort: (int index,bool ascending){
-                    setState(() {
-                      _sortColumnIndex = index;
-                      _sortAscending = ascending;
+                onSort: (int index, bool ascending) {
+                  _postDataSource.sort((post)=> post.title.length,ascending);
+                  setState(() {
+                    _sortColumnIndex = index;
+                    _sortAscending = ascending;
 
-                      posts.sort((a,b){
-                        if(!ascending){
-                          final c = a;
-                          a = b;
-                          b = c;
-                        
-                        }
-                        return a.title.length.compareTo(b.title.length);
-                      });
-                    });
-                    
+                
+                  });
                 },
-                ),
-              DataColumn(label: Text("Author")),
-               DataColumn(label: Text("Image")),
-            ],
-             rows: posts.map((pos){
-               
-               return DataRow(
-                 selected: pos.selected,
-                 onSelectChanged: (bool value){
-                   setState(() {
-                      if(pos.selected != value){
-                      pos.selected = value;
-                    }
-                   });
-                 },
-                 cells: [              
-                   DataCell(Text(pos.title)),
-                   DataCell(Text(pos.author)),
-                   DataCell(
-                     Image.network(pos.imageUrl)
-                   ),
-                 ]
-                 );
-             }).toList(),
               ),
-            ]
-            ),
+              DataColumn(label: Text("Author")),
+              DataColumn(label: Text("Image")),
+            ],
+            // rows: posts.map((pos) {
+            //   return DataRow(
+            //       selected: pos.selected,
+            //       onSelectChanged: (bool value) {
+            //         setState(() {
+            //           if (pos.selected != value) {
+            //             pos.selected = value;
+            //           }
+            //         });
+            //       },
+            //       cells: [
+            //         DataCell(Text(pos.title)),
+            //         DataCell(Text(pos.author)),
+            //         DataCell(Image.network(pos.imageUrl)),
+            //       ]);
+            // }).toList(),
+          ),
+        ]),
       ),
     );
   }
